a51131c1e65f53e279285e2177fa1bc997c92593
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2014-07-25 18:18:29 +0900

    HTTP のリクエストとレスポンスのログを取るメソッドをライブラリ化

--- http.go
+++ http.go
@@ -1,3 +1,18 @@
 package util
 
+import (
+	"net/http"
+	"net/http/httputil"
+)
+
 const ContentTypeJson string = "application/json"
+
+func LogRequest(r *http.Request, body bool) {
+	buff, _ := httputil.DumpRequest(r, body)
+	log.Debug("Request: ", string(buff))
+}
+
+func LogResponse(r *http.Response, body bool) {
+	buff, _ := httputil.DumpResponse(r, true)
+	log.Debug("Response: ", string(buff))
+}
