3d876a0360fbc3e0bae3ce1698020e5ebbe92ae6
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2014-10-07 19:22:27 +0900

    ランダムバイト列の生成をライブラリ化

---
+++ bytes.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"crypto/rand"
+	"github.com/realglobe-Inc/go-lib-rg/erro"
+	"math/big"
+)
+
+func SecureRandomBytes(length int) ([]byte, error) {
+	maxVal := big.NewInt(0).Lsh(big.NewInt(1), uint(length*8))
+	val, err := rand.Int(rand.Reader, maxVal)
+	if err != nil {
+		return nil, erro.Wrap(err)
+	}
+	buff := val.Bytes()
+
+	// 上位 8 ビット以上が全部 0 だと短い値になり得るため。
+	for len(buff) < length {
+		buff = append([]byte{0}, buff...) // ビッグエンディアンなので、前に 0 を足さないと、一様性に影響が出る。
+	}
+
+	return buff, nil
+}
