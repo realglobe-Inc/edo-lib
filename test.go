fefc89a23644f9935f3463383a86b725f26a0675
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2014-10-17 12:36:57 +0900

    go test 用テスト HTTP サーバーを追加

--- test.go
+++ test.go
@@ -3,6 +3,7 @@ package util
 import (
 	"github.com/realglobe-Inc/go-lib-rg/erro"
 	"net"
+	"net/http"
 	"strconv"
 )
 
@@ -24,4 +25,58 @@ func FreePort() (port int, err error) {
 	}
 
 	return port, nil
+}
+
+// テスト用の HTTP サーバー。
+type TestHttpServer struct {
+	lis    net.Listener
+	respCh chan *testHttpServerResponse
+}
+
+type testHttpServerResponse struct {
+	status int
+	header http.Header
+	body   []byte
+}
+
+func NewTestHttpServer(port, chCap int) (*TestHttpServer, error) {
+	lis, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+	if err != nil {
+		return nil, erro.Wrap(err)
+	}
+
+	respCh := make(chan *testHttpServerResponse, chCap)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		resp := <-respCh
+		for key, values := range resp.header {
+			for _, value := range values {
+				w.Header().Add(key, value)
+			}
+		}
+		w.WriteHeader(resp.status)
+		if resp.body != nil {
+			w.Write(resp.body)
+		}
+	})
+
+	go func() {
+		http.Serve(lis, mux)
+	}()
+
+	return &TestHttpServer{lis, respCh}, nil
+}
+
+// 次に返させるレスポンスを与える。
+func (server *TestHttpServer) AddResponse(status int, header http.Header, body []byte) {
+	server.respCh <- &testHttpServerResponse{status, header, body}
+}
+
+// 待ち受けソケットを閉じる。
+func (server *TestHttpServer) Close() error {
+	if err := server.lis.Close(); err != nil {
+		return erro.Wrap(err)
+	}
+	return nil
 }
