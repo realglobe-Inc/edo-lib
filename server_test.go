eb1c5b23990609d424643cfeffbaad7303b3f148
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-01-28 12:41:51 +0900

    プロセス中でサーバーを終了させられるかどうかのテストを追加

---
+++ server_test.go
@@ -0,0 +1,54 @@
+package util
+
+import (
+	"github.com/realglobe-Inc/go-lib-rg/rglog/level"
+	"net/http"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func init() {
+	SetupConsoleLog("github.com/realglobe-Inc", level.OFF)
+}
+
+// サーバーを停止させられるかどうかの検査。
+func TestServerShutdown(t *testing.T) {
+	// ////////////////////////////////
+	// SetupConsoleLog("github.com/realglobe-Inc", level.ALL)
+	// defer SetupConsoleLog("github.com/realglobe-Inc", level.OFF)
+	// ////////////////////////////////
+
+	port, err := FreePort()
+	if err != nil {
+		t.Fatal(err)
+	}
+	shutCh := make(chan struct{}, 10)
+	go TerminableServe("tcp", "", port, "http", map[string]HandlerFunc{"/": func(http.ResponseWriter, *http.Request) error { return nil }}, shutCh)
+	defer func() { shutCh <- struct{}{} }()
+
+	// サーバー起動待ち。
+	time.Sleep(10 * time.Millisecond)
+
+	req, err := http.NewRequest("GET", "http://localhost:"+strconv.Itoa(port)+"/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Connection", "close")
+	if resp, err := (&http.Client{}).Do(req); err != nil {
+		t.Fatal(err)
+	} else if resp.StatusCode != http.StatusOK {
+		t.Error(resp)
+	} else {
+		resp.Body.Close()
+	}
+
+	shutCh <- struct{}{}
+	// サーバー終了待ち。
+	time.Sleep(10 * time.Millisecond)
+
+	if resp, err := (&http.Client{}).Get("http://localhost:" + strconv.Itoa(port) + "/"); err == nil {
+		resp.Body.Close()
+		t.Fatal(err)
+	}
+}
