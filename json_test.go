485194f3e20cae21ffebf53284df3c5c25f24869
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-01-29 22:32:10 +0900

    JSON 向け文字列エスケープ関数のテストを追加

---
+++ json_test.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestJsonStringEscape(t *testing.T) {
+	s := ` !"#$%'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_\` +
+		"`" + `abcdefghijklmnopqrstuvwxyz{|}~` +
+		`いろは` + "\n\r\t\b\f"
+
+	data1, err := json.Marshal(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	data2 := `"` + JsonStringEscape(s) + `"`
+
+	var s1, s2 string
+	if err := json.Unmarshal(data1, &s1); err != nil {
+		t.Fatal(err)
+	}
+	if err := json.Unmarshal([]byte(data2), &s2); err != nil {
+		t.Fatal(err)
+	}
+
+	if s2 != s1 {
+		t.Error(s)
+		t.Error(s2)
+		t.Error(s1)
+	}
+}
