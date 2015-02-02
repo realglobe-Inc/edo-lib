875837228dc724cc598b7faa05f7381c262262e8
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-02-02 13:28:36 +0900

    乱数源を直接読んでも問題無かったので、そうするように修正

--- bytes.go
+++ bytes.go
@@ -3,22 +3,13 @@ package util
 import (
 	"crypto/rand"
 	"github.com/realglobe-Inc/go-lib-rg/erro"
-	"math/big"
+	"io"
 )
 
 func SecureRandomBytes(length int) ([]byte, error) {
-	maxVal := big.NewInt(0).Lsh(big.NewInt(1), uint(length*8))
-	val, err := rand.Int(rand.Reader, maxVal)
-	if err != nil {
+	buff := make([]byte, length)
+	if _, err := io.ReadFull(rand.Reader, buff); err != nil {
 		return nil, erro.Wrap(err)
 	}
-	buff := val.Bytes()
-
-	// 上位 8 ビット以上が全部 0 だと短い値になり得るため。
-	for len(buff) < length {
-		// ビッグエンディアンなので、前に 0 を足さないと、一様性に影響が出る。
-		buff = append([]byte{0}, buff...)
-	}
-
 	return buff, nil
 }
