1a86382566a0704fff9e51039b381da5203a40e0
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-02-03 15:01:06 +0900

    揮発性データコンテナ用キャッシュの並列使用向けテストを追加

--- volatile_key_value_store_cache_test.go
+++ volatile_key_value_store_cache_test.go
@@ -7,3 +7,11 @@ import (
 func TestCachingVolatileKeyValueStore(t *testing.T) {
 	testVolatileKeyValueStore(t, newCachingVolatileKeyValueStore(newMemoryVolatileKeyValueStore(0, 0)))
 }
+
+func TestCachingConcurrentVolatileKeyValueStore(t *testing.T) {
+	testConcurrentVolatileKeyValueStore(t, newCachingVolatileKeyValueStore(newMemoryVolatileKeyValueStore(testStaleDur, testCaExpiDur)))
+}
+
+func TestCachingConcurrentVolatileKeyValueStoreConsistency(t *testing.T) {
+	testConcurrentVolatileKeyValueStoreConsistency(t, newSynchronizedVolatileKeyValueStore(newCachingVolatileKeyValueStore(newMemoryVolatileKeyValueStore(testStaleDur, testCaExpiDur))))
+}
