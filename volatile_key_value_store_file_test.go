bac370dd25396c6e013bf7183e663cb6b221087e
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-02-03 15:02:47 +0900

    ファイルベース揮発性データコンテナの並列使用向けテストを追加

--- volatile_key_value_store_file_test.go
+++ volatile_key_value_store_file_test.go
@@ -2,12 +2,22 @@ package driver
 
 import (
 	"encoding/json"
+	"github.com/realglobe-Inc/edo/util"
+	"github.com/realglobe-Inc/go-lib-rg/rglog/level"
 	"io/ioutil"
 	"os"
 	"testing"
 )
 
+func init() {
+	util.SetupConsoleLog("github.com/realglobe-Inc", level.OFF)
+}
+
 func TestFileVolatileKeyValueStore(t *testing.T) {
+	// ////////////////////////////////
+	// util.SetupConsoleLog("github.com/realglobe-Inc", level.ALL)
+	// defer util.SetupConsoleLog("github.com/realglobe-Inc", level.OFF)
+	// ////////////////////////////////
 	path, err := ioutil.TempDir("", testLabel)
 	if err != nil {
 		t.Fatal(err)
@@ -19,5 +29,39 @@ func TestFileVolatileKeyValueStore(t *testing.T) {
 	}
 	defer os.RemoveAll(expiPath)
 
-	testVolatileKeyValueStore(t, newFileVolatileKeyValueStore(path, expiPath, nil, nil, json.Marshal, jsonUnmarshal, 0, 0))
+	testVolatileKeyValueStore(t, newFileVolatileKeyValueStore(path, expiPath, nil, nil, json.Marshal, jsonUnmarshal, testStaleDur, testCaExpiDur))
+}
+
+func TestFileConcurrentVolatileKeyValueStore(t *testing.T) {
+	path, err := ioutil.TempDir("", testLabel)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(path)
+	expiPath, err := ioutil.TempDir("", testLabel)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(expiPath)
+
+	testConcurrentVolatileKeyValueStore(t, newFileVolatileKeyValueStore(path, expiPath, nil, nil, json.Marshal, jsonUnmarshal, testStaleDur, testCaExpiDur))
+}
+
+func TestFileConcurrentVolatileKeyValueStoreConsistency(t *testing.T) {
+	// ////////////////////////////////
+	// util.SetupConsoleLog("github.com/realglobe-Inc", level.ALL)
+	// defer util.SetupConsoleLog("github.com/realglobe-Inc", level.OFF)
+	// ////////////////////////////////
+	path, err := ioutil.TempDir("", testLabel)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(path)
+	expiPath, err := ioutil.TempDir("", testLabel)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(expiPath)
+
+	testConcurrentVolatileKeyValueStoreConsistency(t, NewFileVolatileKeyValueStore(path, expiPath, nil, nil, json.Marshal, jsonUnmarshal, testStaleDur, testCaExpiDur))
 }
